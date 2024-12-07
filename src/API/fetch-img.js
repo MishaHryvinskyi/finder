cd60942eb9535dcbc7f88f04a19fda6574716914
Misha <hryvinskyim@gmail.com>
2024-12-07 11:50:04 +0200

    start pagination

--- src/API/fetch-img.js
+++ src/API/fetch-img.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const API = 'https://pixabay.com/api/'
 const API_KEY = '35643945-433c06e40cd86730ec72beccd';
 
-async function getPhotos(q) {
-    const response = await axios.get(`${API}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`);
+async function getPhotos(q, page = 1) {
+    const response = await axios.get(`${API}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
     return response.data;
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,30 @@ import { markupGallery } from "./JS/markup";
 const form = document.querySelector('#search-form');
 const inputEl = document.querySelector('input[name="searchQuery"]');
 const galleryEl = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.js-load');
+
+
+let currentPage = 1;
 
 form.addEventListener('submit', onSubmit)
+btnLoadMore.addEventListener('click', onLoad);
+
+function onLoad() {
+    currentPage += 1;
+    getPhotos(currentPage).then(data => {
+        const markup = markupGallery(data.hits);
+        galleryEl.insertAdjacentHTML('beforeend', markup);
+    });
+}
 
 function onSubmit(event) {
     event.preventDefault();
-    
+
     const query = inputEl.value.trim();
 
     getPhotos(query).then(data => {
         const markup = markupGallery(data.hits);
-        galleryEl.innerHTML= markup;
+        galleryEl.insertAdjacentHTML('beforeend', markup);
+        btnLoadMore.hidden = false;
     }).catch(e => console.log(e)).finally(form.reset());
 }
