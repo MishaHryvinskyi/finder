21a6159cb920486b3039f8450031be2d694ae8dd
Misha <hryvinskyim@gmail.com>
2024-12-08 15:31:19 +0200

    added SimpleLightBox

--- src/JS/markup.js
+++ src/JS/markup.js
@@ -9,21 +9,23 @@ function markupGallery(data) {
         downloads 
     }) => `
     <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-            <p class="info-item">
-            <b>Likes: ${likes}</b>
-            </p>
-            <p class="info-item">
-            <b>Views: ${views}</b>
-            </p>
-            <p class="info-item">
-            <b>Comments: ${comments}</b>
-            </p>
-            <p class="info-item">
-            <b>Downloads: ${downloads}</b>
-            </p>
-        </div>
+        <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes: ${likes}</b>
+                </p>
+                <p class="info-item">
+                <b>Views: ${views}</b>
+                </p>
+                <p class="info-item">
+                <b>Comments: ${comments}</b>
+                </p>
+                <p class="info-item">
+                <b>Downloads: ${downloads}</b>
+                </p>
+            </div>
+        </a>
     </div>
 `).join('')
 };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { getPhotos } from "./API/fetch-img";
 import { markupGallery } from "./JS/markup";
 
-
 const form = document.querySelector('#search-form');
 const inputEl = document.querySelector('input[name="searchQuery"]');
 const galleryEl = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.js-load');
 
+const simplelightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+})
 
 let currentPage = 1;
 let currentQery = '';
@@ -20,13 +24,13 @@ btnLoadMore.addEventListener('click', onLoad);
 
 function onLoad() {
     currentPage += 1;
-    getPhotos(currentQery, currentPage).then(data => {
-        galleryEl.insertAdjacentHTML('beforeend', markupGallery(data.hits));
-        if(data.totalHits === data.total) {
+    getPhotos(currentQery, currentPage).then(({ total, totalHits, hits }) => {
+        galleryEl.insertAdjacentHTML('beforeend', markupGallery(hits));
+        simplelightbox.refresh();
+        if(totalHits === total) {
             btnLoadMore.hidden = true;
         }  
-        // .catch(onError);
-    });
+    }).catch(onError);
 }
 
 function onSubmit(event) {
@@ -44,9 +48,10 @@ function onSubmit(event) {
     }
 
     getPhotos(currentQery)
-    .then(data => {
-        galleryEl.insertAdjacentHTML('beforeend', markupGallery(data.hits));
-        if(data.totalHits !== data.total) {
+    .then(({ totalHits, total, hits }) => {
+        galleryEl.insertAdjacentHTML('beforeend', markupGallery(hits));
+        simplelightbox.refresh();
+        if(totalHits !== total) {
             btnLoadMore.hidden = false;
         }  
     }).catch(onError);
